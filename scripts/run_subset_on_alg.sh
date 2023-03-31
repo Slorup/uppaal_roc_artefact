c567a849431ad29182d3c6811f1695bcd426bb4e
Slorup <nsjo18@student.aau.dk>
2023-03-31 13:03:07 +0200

    Fixed problem if folder did not exist

--- scripts/run_subset_on_alg.sh
+++ scripts/run_subset_on_alg.sh
@@ -6,11 +6,6 @@ FILTER="${3}"
 UPPAAL_FOLDER="${4}"
 count=1
 
-echo $ALG
-echo $MAX_INSTANCES_TO_RUN
-echo $FILTER
-echo $UPPAAL_FOLDER
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ARTEFACT_DIR="$(dirname "$SCRIPT_DIR")"
 
@@ -23,6 +18,9 @@ declare -A algToGitBranchName
 algToGitBranchName["concretemcr"]="roc"
 algToGitBranchName["concretemcr_por"]="por"
 algToGitBranchName["lambdadeduction"]="lambdadeduction"
+
+cd $ARTEFACT_DIR
+mkdir -p results/$ALG
 
 echo Switching to git branch: ${algToGitBranchName["${ALG}"]}
 cd $UPPAAL_FOLDER
